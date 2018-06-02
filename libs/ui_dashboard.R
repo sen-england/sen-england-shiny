43e5de21d2dc4149290fa561301fac2ae8e4c1d6
YiLiu6240 <yi.liu6240@gmail.com>
2018-06-02 18:30:00 +0100

    Hack dashboardHeader

--- libs/ui_dashboard.R
+++ libs/ui_dashboard.R
@@ -2,7 +2,29 @@
 # NOTE: font-awesome icons are version 4
 
 # ==== Individual components ====
-dsb_header <- dashboardHeader(title = "SEN England")
+dashboardHeaderHack <- function (..., title = NULL, longtitle = NULL,
+                                 disable = FALSE, .list = NULL) {
+  items <- c(list(...), .list)
+  lapply(items, shinydashboard:::tagAssert, type = "li", class = "dropdown")
+  tags$header(
+    class = "main-header",
+    style = if (disable) "display: none;",
+    span(class = "logo", title),
+    tags$nav(class = "navbar navbar-static-top",
+             role = "navigation",
+             span(shiny::icon("bars"),
+                  style = "display:none;"),
+             a(href = "#", class = "sidebar-toggle",
+               `data-toggle` = "offcanvas", role = "button",
+               span(class = "sr-only", "Toggle navigation")),
+             div(class = "longtitle", longtitle),
+             div(class = "navbar-custom-menu",
+                 tags$ul(class = "nav navbar-nav",
+                         items))))
+}
+dsb_header <- dashboardHeaderHack(
+  title = "SEN & academisation",
+  longtitle = "Inclusion and the academisation of English schools")
 dsb_id_primary <- "primary"
 dsb_id_tseries <- "tseries"
 dsb_id_maps <- "maps"
@@ -43,6 +65,8 @@ dsb_sidebar <- dashboardSidebar(
 # ==== Main tabs ====
 
 dsb_main <- dashboardBody(
+  tags$head(tags$link(rel = "stylesheet",
+                      type = "text/css", href = "custom.css")),
   tabItems(tabItem(tabName = dsb_id_primary,
                    panel_primary),
            tabItem(tabName = dsb_id_tseries,
