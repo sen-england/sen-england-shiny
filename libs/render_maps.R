5fb68db8992da6a1bde1fbc197b8c16cfe168d73
YiLiu6240 <yi.liu6240@gmail.com>
2018-06-25 13:20:01 +0100

    Refactor df summarisation out of map rendering

--- libs/render_maps.R
+++ libs/render_maps.R
@@ -1,62 +1,63 @@
-render_map_academ <- function(year, shape, df_send, region,
-                              auto_breaks = FALSE) {
-  message(glue("{Sys.time()}, start rendering `map_academ`"))
+summarise_map_df <- function(df_send, shape, year, type, region, sen_type) {
+  df <- df_send %>%
+    filter(Year == year) %>%
+    filter(RegionCode %in% region)
+
+  if (type == "Academisation") {
+    df <- df %>% select(Year, LACode, IsAcademy) %>%
+      collect() %>%
+      summarise_academ(by = "LACode", multiplier = FALSE)
+  } else {
+    df <- df %>%
+      select(Year, LACode,
+             SEN_Support, Statement_EHC_Plan, TotalPupils) %>%
+      collect() %>%
+      summarise_sen(sen_type, by = "LACode", multiplier = FALSE)
+  }
+
   shape@data <- shape@data %>%
-    left_join(
-      df_send %>%
-        filter(Year == year) %>%
-        filter(RegionCode %in% region) %>%
-        select(Year, LACode, IsAcademy) %>%
-        collect() %>%
-        summarise_academ(by = "LACode", multiplier = FALSE),
-      by = c("code" = "LACode"))
+    left_join(df, by = c("code" = "LACode"))
+  shape
+}
+
+render_map_academ <- function(year, shape, auto_breaks = FALSE) {
+  message(glue("{Sys.time()}, start rendering `map_academ`"))
   map_academ_plot <- tmap::tm_shape(shape[!is.na(shape$Academies), ]) +
     tmap::tm_polygons("Academies", id = "name",
-                palette = params$maps_academ$palette,
-                breaks = if (auto_breaks) {
-                  NULL
-                } else {
-                  params$maps_academ$breaks
-                }) +
+                      palette = params$maps_academ$palette,
+                      breaks = if (auto_breaks) {
+                        NULL
+                      } else {
+                        params$maps_academ$breaks
+                      }) +
     tmap::tm_legend(legend.format = list(fun = prop_to_pct))
   message(glue("{Sys.time()}, finished rendering `map_academ`"))
   withProgress(
     tmap::tmap_leaflet(map_academ_plot, mode = "view", show = TRUE),
-    message = "Rendering maps... Please wait")
-
+    message = "Rendering maps...")
 }
 
-render_map_sen <- function(year, shape, df_send, sen_type, region,
-                           auto_breaks = FALSE) {
+render_map_sen <- function(year, shape, sen_type, auto_breaks = FALSE) {
 
   message(glue("{Sys.time()}, start rendering `map_sen`"))
-  shape@data <- shape@data %>%
-    left_join(
-      df_send %>%
-        filter(Year == year) %>%
-        filter(RegionCode %in% region) %>%
-        select(Year, LACode,
-               SEN_Support, Statement_EHC_Plan, TotalPupils) %>%
-        collect() %>%
-        summarise_sen(sen_type, by = "LACode", multiplier = FALSE),
-      by = c("code" = "LACode"))
   map_sen_plot <- tmap::tm_shape(shape[!is.na(shape$SEN), ]) +
     tmap::tm_polygons("SEN", id = "name",
-                palette = params$maps_sen$palette,
-                breaks = if (auto_breaks) {
-                  NULL
-                } else {
-                  params$maps_sen$breaks
-                }) +
+                      palette = params$maps_sen$palette,
+                      breaks = if (auto_breaks) {
+                        NULL
+                      } else {
+                        params$maps_sen$breaks
+                      }) +
     tmap::tm_legend(legend.format = list(fun = prop_to_pct))
   message(glue("{Sys.time()}, finished rendering `map_sen`"))
   withProgress(
     tmap::tmap_leaflet(map_sen_plot, mode = "view", show = TRUE),
-    message = "Rendering maps... Please wait")
+    message = "Rendering maps...")
 }
 
 render_map <- function(year, shape, df_send,
                        type = c("Academisation", "SEN"),
+                       region_type = c("LA", "ParlCon"),
                        sen_type = c("SEN_Support",
                                     "Statement_EHC_Plan"),
                        region = c("E12000007", "E12000003", "E12000009",
@@ -64,11 +65,15 @@ render_map <- function(year, shape, df_send,
                                   "E12000008", "E12000001", "E12000004"),
                        auto_breaks = FALSE) {
   type <- match.arg(type)
+  region_type <- match.arg(region_type)
+
+  shape <- withProgress(
+    summarise_map_df(df_send, shape, year, type, region, sen_type),
+    message = "Collecting data...")
+
   if (type == "Academisation") {
-    render_map_academ(year, shape, df_send, region,
-                      auto_breaks)
+    render_map_academ(year, shape, auto_breaks)
   } else {
-    render_map_sen(year, shape, df_send, sen_type, region,
-                   auto_breaks)
+    render_map_sen(year, shape, sen_type, auto_breaks)
   }
 }
