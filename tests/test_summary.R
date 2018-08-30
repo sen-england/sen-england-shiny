1ba6f574570573a8007133e13cf9f760ca7598e3
YiLiu6240 <yi.liu6240@gmail.com>
2018-08-30 09:31:48 +0100

    Add test summary routines

---
+++ tests/test_summary.R
@@ -0,0 +1,18 @@
+suppressMessages(suppressWarnings({
+  library("testthat")
+  library("tidyverse")
+  library("here")
+}))
+
+source(here("libs/common.R"), local = TRUE)
+
+test_that("libs/common.R: `summarise_academ`", {
+
+})
+
+test_that("libs/common.R: `summarise_sen`", {
+
+})
+
+# TODO: break down routines in render_value_box.R
+#       and render_primary.R to summarisation routines and rendering routines
