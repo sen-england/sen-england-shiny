d22c493d6bd77488c4398eca74b508a2ad5a6304
YiLiu6240 <yi.liu6240@gmail.com>
2018-09-03 16:03:56 +0100

    Update unit testing routines

--- tests/test_summary.R
+++ tests/test_summary.R
@@ -88,28 +88,43 @@ test_that("`summarise_academ`: multiplier", {
 })
 
 test_that("`summarise_academ`: summarisation", {
-  df <- tibble(ID = c("A", "B", "C") %>% rep(each = 3),
-               IsAcademy = c(c(1, 0, 1),
-                             c(0, 1, 0),
-                             c(1, 1, 1)))
+  df <- tribble(
+    ~ID, ~IsAcademy,
+    "A", 1,
+    "A", 0,
+    "A", 1,
+    "B", 0,
+    "B", 1,
+    "B", 0,
+    "C", 1,
+    "C", 1,
+    "C", 1
+  )
   df_outcome <- df %>%
     summarise_academ(by = "ID", multiplier = FALSE)
-  df_expected <- tibble(ID = c("A", "B", "C"),
-                        Academies = c(2/3, 1/3, 1))
+  df_expected <- tribble(
+    ~ID, ~Academies,
+    "A", 2/3,
+    "B", 1/3,
+    "C", 1
+  )
   expect_true(all.equal(df_outcome, df_expected))
 })
 
 test_that("`summarise_academ`: summarisation with `by_academisation_route`", {
-  df <- tibble(
-    ID = c("A", "B") %>% rep(each = 5),
-    TypeAcademy_code = c(
-      c("ca", "sa", "foobar", "ca", "ca"),
-      c("sa", "sa", "ca", "ca", "foobar")),
-    TypeAcademy = if_else(TypeAcademy_code == "ca", "converter academy",
-                          if_else(TypeAcademy_code == "sa", "sponsored academy",
-                                  TypeAcademy_code)),
-    IsAcademy = c(c(1, 0, 1, 0, 1),
-                  c(1, 1, 1, 1, 1)))
+  df <- tribble(
+    ~ID, ~TypeAcademy,        ~IsAcademy,
+    "A", "converter academy", 1,
+    "A", "sponsored academy", 0,
+    "A", "foobar",            1,
+    "A", "converter academy", 0,
+    "A", "converter academy", 1,
+    "B", "sponsored academy", 1,
+    "B", "sponsored academy", 1,
+    "B", "converter academy", 1,
+    "B", "converter academy", 1,
+    "B", "foobar",            1
+  )
   df_outcome <- df %>%
     summarise_academ(by = "ID", multiplier = FALSE,
                      by_academisation_route = TRUE)
@@ -118,12 +133,88 @@ test_that("`summarise_academ`: summarisation with `by_academisation_route`", {
     "A", "Sponsored Academy", 0,
     "B", "Sponsored Academy", 0.4,
     "A", "Converter Academy", 0.4,
-    "B", "Converter Academy", 0.4)
+    "B", "Converter Academy", 0.4
+  )
   expect_true(all.equal(df_outcome, df_expected))
 })
 
-# WIP
 context("libs/common.R: `summarise_sen`")
+
+test_that("`summarise_sen`: structure", {
+  expected_names <- c("Year", "Phase", "SEN")
+  names <- df_main %>%
+    summarise_sen(by = c("Year", "Phase"),
+                  by_sen_type = FALSE) %>%
+    names()
+  expect_equal(names, expected_names)
+})
+
+test_that("`summarise_sen`: structure with `by_sen_type`", {
+  expected_names <- c("Year", "Phase", "TypeSEN", "SEN")
+  names <- df_main %>%
+    summarise_sen(by = c("Year", "Phase"),
+                  by_sen_type = TRUE) %>%
+    names()
+  expect_equal(names, expected_names)
+})
+
+test_that("`summarise_sen`: multiplier", {
+  prop_value <- df_main %>%
+    summarise_sen(by = c("RegionCode"),
+                  multiplier = FALSE) %$%
+    SEN
+  pct_value <- df_main %>%
+    summarise_sen(by = c("RegionCode"),
+                  multiplier = TRUE) %$%
+    SEN
+  expect_equal(prop_value * 100, pct_value)
+})
+
+test_that("`summarise_sen`: summarisation", {
+  df <- tribble(
+    ~ID, ~Group, ~TotalPupils, ~SEN_Support, ~Statement_EHC_Plan,
+    1,   1,      100,          20,           5,
+    2,   2,      30,           5,            2,
+    3,   1,      150,          20,           10,
+    4,   2,      120,          15,           5
+  )
+  test_that("sen_type: SEN_Support and Statement_EHC_Plan", {
+    df_outcome <- df %>%
+      summarise_sen(sen_type = c("SEN_Support", "Statement_EHC_Plan"),
+                    by = "Group", by_sen_type = FALSE,
+                    multiplier = FALSE)
+    df_expected <- tribble(
+      ~Group, ~SEN,
+      1,      55/250,
+      2,      27/150
+    )
+    expect_true(all.equal(df_outcome, df_expected))
+  })
+  test_that("sen_type: SEN_Support", {
+    df_outcome <- df %>%
+      summarise_sen(sen_type = c("SEN_Support"),
+                    by = "Group", by_sen_type = FALSE,
+                    multiplier = FALSE)
+    df_expected <- tribble(
+      ~Group, ~SEN,
+      1,      40/250,
+      2,      20/150
+    )
+    expect_true(all.equal(df_outcome, df_expected))
+  })
+  test_that("sen_type: EHC", {
+    df_outcome <- df %>%
+      summarise_sen(sen_type = c("Statement_EHC_Plan"),
+                    by = "Group", by_sen_type = FALSE,
+                    multiplier = FALSE)
+    df_expected <- tribble(
+      ~Group, ~SEN,
+      1,      15/250,
+      2,      7/150
+    )
+    expect_true(all.equal(df_outcome, df_expected))
+  })
+})
 
 # TODO: break down routines in render_value_box.R
 #       and render_primary.R to summarisation routines and rendering routines
