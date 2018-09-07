2f040ea1cd7e7bb41e0e41043417091743abdfc2
YiLiu6240 <yi.liu6240@gmail.com>
2018-09-07 15:05:00 +0100

    Add test for data query

---
+++ tests/test_data_query.R
@@ -0,0 +1,145 @@
+suppressMessages(suppressWarnings({
+  library("testthat")
+  library("tidyverse")
+  library("magrittr")
+  library("here")
+}))
+
+source(here("libs/common.R"), local = TRUE)
+
+data_conf <- config::get("data")
+send_db_conf <- data_conf$send_db
+preproc_conf <- data_conf$preprocess
+params <- config::get("params")
+
+send_db_conn <- DBI::dbConnect(
+  RSQLite::SQLite(),
+  dbname = here(send_db_conf$db))
+preproc_db_conn <- DBI::dbConnect(
+  RSQLite::SQLite(),
+  dbname = here(preproc_conf$db))
+df_main_table <- send_db_conn %>%
+  tbl(send_db_conf$tbl) %>%
+  select(one_of(send_db_conf$vars))
+
+dsb_id_primary <- "primary"
+dsb_id_tseries <- "tseries"
+dsb_id_maps <- "maps"
+cand_years <- 2011L:2017L
+cand_types <- c("% academised schools" = "Academisation",
+                "% pupils with SEN" = "SEN")
+cand_phases <- c("Primary schools" = "primary",
+                 "Secondary schools" = "secondary",
+                 "Others (e.g. nursery, 16 Plus, etc)" = "others")
+cand_type_sen <- c("SEN_Support", "Statement_EHC_Plan") %>%
+  set_names(str_replace_all(., "_", " "))
+cand_type_schools <- c("Mainstream School" = "mainstream school",
+                       "Pupil Referral Unit" = "pupil referral unit",
+                       "Special School" = "special school",
+                       "Others (e.g. independent school)" = "others")
+cand_la_tbl <- read_csv(here("data/region-info/region-info.csv"),
+                        col_types = c("cccc"))
+cand_la <- cand_la_tbl %>%
+  select(LAName, LACode) %>% distinct() %>% deframe()
+cand_region <- cand_la_tbl %>%
+  select(RegionName, RegionCode) %>% distinct() %>% deframe()
+cand_parlcon <- read_csv(here("data/region-info/parlcon-info.csv"),
+                         col_types = c("cc")) %>% deframe()
+
+context("Combination of candidates")
+
+combine_candidates <- function(candidates) {
+  # For a vector of candidates, generate a combination of themselves.
+  # i.e. for c("A", "B", "C"), get
+  # - "A"; "B"; "C"; "A", "B"; "A", "C"; "B", "C"; "A", "B", "C"
+  1:length(candidates) %>%
+    map(function(m) partial(combn, m = m, simplify = FALSE,
+                            .lazy = FALSE)) %>%
+    invoke_map(x = candidates) %>%
+    purrr::flatten()
+}
+
+test_that("`combine_candidates`", {
+  outcome <- c("A", "B", "C") %>% combine_candidates()
+  expected_outcome <- list(c("A"), c("B"), c("C"),
+                           c("A", "B"), c("A", "C"), c("B", "C"),
+                           c("A", "B", "C"))
+  expect_equal(outcome, expected_outcome)
+})
+
+context(paste0("Global arguments: reactive loadings of dataframes ",
+               "under all argument candidates"))
+
+args <- cross(list(
+  Phase = cand_phases %>% combine_candidates(),
+  TypeGeneral = cand_type_schools %>% combine_candidates()
+))
+
+test_that("`df_main`", {
+  expect_silent({
+    args %>%
+      map(function(arg) {
+        df_main <- df_main_table %>%
+          filter(Phase %in% arg$Phase,
+                 TypeGeneral %in% arg$TypeGeneral) %>%
+          head() %>% collect()
+        df_main
+      })
+  })
+})
+
+test_that("`df_preproc_stats_sen`", {
+  expect_silent({
+    args %>%
+      map(function(arg) {
+        df_preproc_stats_sen <- preproc_db_conn %>%
+          tbl(preproc_conf$stats_sen) %>%
+          filter(Phase %in% arg$Phase,
+                 TypeGeneral %in% arg$TypeGeneral) %>%
+          head() %>% collect()
+        df_preproc_stats_sen
+      })
+  })
+})
+
+test_that("`df_preproc_stats_schools`", {
+  expect_silent({
+    args %>%
+      map(function(arg) {
+        df_preproc_stats_schools <- preproc_db_conn %>%
+          tbl(preproc_conf$stats_schools) %>%
+          filter(Phase %in% arg$Phase,
+                 TypeGeneral %in% arg$TypeGeneral) %>%
+          head() %>% collect()
+        df_preproc_stats_schools
+      })
+  })
+})
+
+test_that("`df_preproc_composition_sen`", {
+  expect_silent({
+    args %>%
+      map(function(arg) {
+        df_preproc_composition_sen <- preproc_db_conn %>%
+          tbl(preproc_conf$composition_sen) %>%
+          filter(Phase %in% arg$Phase,
+                 TypeGeneral %in% arg$TypeGeneral) %>%
+          head() %>% collect()
+        df_preproc_composition_sen
+      })
+  })
+})
+
+test_that("`df_preproc_composition_schools`", {
+  expect_silent({
+    args %>%
+      map(function(arg) {
+        df_preproc_composition_schools <- preproc_db_conn %>%
+          tbl(preproc_conf$composition_schools) %>%
+          filter(Phase %in% arg$Phase,
+                 TypeGeneral %in% arg$TypeGeneral) %>%
+          head() %>% collect()
+        df_preproc_composition_schools
+      })
+  })
+})
